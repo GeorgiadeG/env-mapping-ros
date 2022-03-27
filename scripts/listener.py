99f11b2f2b5653417be6329db41adb4540ba86b4
Ioannis Georgiades <I.GEORGIADES@student.tudelft.nl>
2022-03-28 01:33:47 +0200

    fix initial angle

--- scripts/listener.py
+++ scripts/listener.py
@@ -14,6 +14,9 @@ GREEN = (0, 255, 0)
 GRAY = (150, 150, 150)
 WHITE = (255, 255, 255)
 
+#define PI
+PI = 3.14159265358979
+
 squareSize = 6
 
 pygame.init()
@@ -21,7 +24,7 @@ screen = pygame.display.set_mode(SIZE)
 
 def callback(data):
 	screen.fill(WHITE)
-	currAngle = 0
+	currAngle = data.angle_min
 	angleIncr = data.angle_increment
 	for l in data.ranges:
 		if (l >= data.range_min and l <= data.range_max):
